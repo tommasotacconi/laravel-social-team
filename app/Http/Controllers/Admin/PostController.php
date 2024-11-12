4d02f1623c3619c0e50be58e1fe4949a03836dd6
AndreaMargutti <marguttiandrea30@gmail.com>
2024-11-12 16:46:16 +0100

    completed index

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     //
     public function index () {
+        $posts = Post::all();
 
+        return view ('admin.posts.index', compact('posts'));
     }
 
     public function show () {
