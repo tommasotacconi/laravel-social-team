6003dc2d816e42e8167b46614bb6d62b4e1b94a1
AndreaMargutti <marguttiandrea30@gmail.com>
2024-11-12 16:48:02 +0100

    remove model

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Http\Request\UpdatePostsRequest;
