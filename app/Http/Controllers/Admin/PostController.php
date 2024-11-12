f0de2119c43c167cb53b68faaa90baf44d8fe5eb
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-11-12 16:35:46 +0100

    Add Update fuction in Admin Post Controller.

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Http\Request\UpdatePostsRequest;
 
 class PostController extends Controller
 {
@@ -28,8 +30,20 @@ class PostController extends Controller
 
     }
 
-    public function update () {
+    public function update (UpdatePostsRequest $request, string $id) {
 
+        $request->validated();
+
+        $newData = $request->all();
+
+        $post = Post::findOrFail($id);
+        $post->title = $newData["title"];
+        $post->author = $newData["author"];
+        $post->description = $newData["description"];
+
+        $post->save();
+
+        return redirect()->route("admin.posts.show", [ "id"=> $post->id] );
     }
 
     public function destroy () {
